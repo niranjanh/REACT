9eda326d4798bdc2db59d6115086578be2ceb86e
niranjanh <niranjan.harpale@routerabbit.com>
2020-06-03 22:14:55 +0530

    class based component practice react

---
+++ 03_Class-based_Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Name from "./Name.js";
+
+class App extends React.Component {
+    
+    render() {
+        return (
+            <Name />
+        )
+    }
+}
+
+export default App
+
---
+++ 03_Class-based_Components/Name.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Name extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Name: Niranjan Harpale</h1>
+            </div>
+        )
+    }
+}
+
+export default Name;
